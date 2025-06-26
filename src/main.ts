e00162f83ff9245189fc7dc90a17b57f57c33941
Marco Swart <marco.swart959@gmail.com>
2025-06-26 16:14:49 +0200

    Changed camera position

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ import { createRenderer } from './renderer'
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.set(10, 10, 10)
+camera.position.set(70, 70, 70)
 
 const light = createLight(camera)
 scene.add(light)
